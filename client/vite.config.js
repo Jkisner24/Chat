82a1d39df05f3dedd026e79e32830ddae621e607
Julian Kisner <julikisner@gmail.com>
2023-10-04 14:43:32 -0300

    change port

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/socket.io': {
-        target: 'http://localhost:3001',
+        target: 'http://localhost:80',
         ws: 'true'
       }
     }
