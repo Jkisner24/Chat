383e60e49468a6332ff9b540334d7e51d9f44a3d
Julian Kisner <julikisner@gmail.com>
2023-10-05 12:06:24 -0300

    local change port

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/socket.io': {
-        target: 'https://chat-umgd.onrender.com',
+        target: 'http://localhost:80',
         ws: 'true'
       }
     }
