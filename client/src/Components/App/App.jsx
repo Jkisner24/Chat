dc4738ad3be638d80f04cdfbea01b55d44d1d3b7
Julian Kisner <julikisner@gmail.com>
2023-10-06 19:29:23 -0300

    change button

--- client/src/Components/App/App.jsx
+++ client/src/Components/App/App.jsx
@@ -43,27 +43,44 @@ function App() {
     })
   }
 
-  return(
-    <div className={style.chats} >
-      <form onSubmit={handleSubmit} > 
-        <input value={message} onChange={handleInput} type='text' placeholder='Write your message' />
+  return (
+    <div className={style.chats}>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={message}
+          onChange={handleInput}
+          type="text"
+          placeholder="Write your message"
+        />
         <Button type="submit" variant="info mt-1">
-          send
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            class="bi bi-send"
+            viewBox="0 0 16 16"
+          >
+            <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z" />
+          </svg>
         </Button>
       </form>
-      <ListGroup className='mb-2'>
-        {
-          messages.map((message, i) =>
-            <ListGroup.Item 
-              key={i} 
-              className={`mb-1 text-dark ${message.from !== username ? 'text-start &&  text-secondary' : 'text-end && text-white-50 bg-dark'}`}
-            >
-            {message.from}:{message.body}</ListGroup.Item>
-          )
-        }
+      <ListGroup className="mb-2">
+        {messages.map((message, i) => (
+          <ListGroup.Item
+            key={i}
+            className={`mb-1 text-dark ${
+              message.from !== username
+                ? "text-start &&  text-secondary"
+                : "text-end && text-white-50 bg-dark"
+            }`}
+          >
+            {message.from}:{message.body}
+          </ListGroup.Item>
+        ))}
       </ListGroup>
     </div>
-  )
+  );
 }
 
 export default App
